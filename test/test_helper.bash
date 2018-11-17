9023397347cd3c393fdc020c9d290a1f2cb0507c
Jason Karns <jason@karns.name>
2018-11-17 13:16:57 -0500

    Add nodenv as npm devDep instead of cloning for test suite

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -5,22 +5,13 @@ load ../node_modules/bats-assert/all
 unset NODENV_VERSION
 unset NODENV_DIR
 
-NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
-
 # guard against executing this block twice due to bats internals
-if [ "$NODENV_ROOT" != "${NODENV_TEST_DIR}/root" ]; then
-  export NODENV_ROOT="${NODENV_TEST_DIR}/root"
-  export HOME="${NODENV_TEST_DIR}/home"
+if [ "$NODENV_ROOT" != "${BATS_TMPDIR}/root" ]; then
+  export HOME="${BATS_TMPDIR}/home"
+  export NODENV_ROOT="${BATS_TMPDIR}/root"
 
-  PATH=/usr/bin:/bin:/usr/sbin:/sbin
-  PATH="${NODENV_TEST_DIR}/bin:$PATH"
+  PATH="$(npm bin):/usr/bin:/bin:/usr/sbin:/sbin"
   PATH="${BATS_TEST_DIRNAME}/../bin:$PATH"
-  PATH="${BATS_TEST_DIRNAME}/../nodenv/libexec:$PATH"
-  PATH="${BATS_TEST_DIRNAME}/../nodenv/test/libexec:$PATH"
   PATH="${NODENV_ROOT}/shims:$PATH"
   export PATH
 fi
-
-teardown() {
-  rm -rf "$NODENV_TEST_DIR"
-}
